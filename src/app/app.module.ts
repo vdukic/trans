7df1ffbfda0ce599808cbb327d953c33ccadf7fa
vladimir86 <vdukic1586@gmail.com>
2017-11-05 20:22:52 +0100

    Tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {AppComponent} from './app.component';
+import {CompanyUsersComponent} from './company-users/company-users.component';
+import {SettingsComponent} from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyUsersComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: CompanyUsersComponent
+    }, {
+      path: 'companyUsers',
+      component: CompanyUsersComponent
+    },
+      {
+        path: 'settings',
+        component: SettingsComponent
+      }])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
